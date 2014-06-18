94f54dffed45cf6c3aa42ff66bfdd35461bd3cbd
DonaldTank <d.barkhatov@list.ru>
2014-06-18 17:24:06 +0300

    3 переменные

---
+++ variable/variable.php
@@ -0,0 +1,10 @@
+<?php
+$boolean = true;
+$integer = 5;
+$float = 3.1415;
+$string = "Text";
+echo "\$string = ". $string;// вывод на экран $boolean = Текст
+echo "<br>\$boolean = ". $boolean;
+echo "<br>\$float = ". $float
+?>
+

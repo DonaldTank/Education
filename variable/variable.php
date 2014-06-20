5a68afad95c7ab370d7d3273809d6afd6beff7ab
DonaldTank <d.barkhatov@list.ru>
2014-06-21 00:20:58 +0300

    Преобразовать в бул: ** int ** float ** string ** array * Преобразовать бул в инт * Преобразовать бул в строку при помощи оператора IF (остались вопросы) прочитать все про оператор IF прочитать все про оператор switch case

--- variable/variable.php
+++ variable/variable.php
@@ -1,10 +1,9 @@
 <?php
 $boolean = true;
-$integer = 67;
+$integer = 0;
 $float = 3.1415;
 $string = "Text";
 $array = array('0' => 'o', '1' => 'r', '2' => 'a', '3' => 'n', '4' => 'g', '5' => 'e');
-print_r($array);
 //можно и так, но громоздко:
 //$array[0]="o";
 //$array[1]="r";
@@ -12,25 +11,43 @@ print_r($array);
 //$array[3]="n";
 //$array[4]="g";
 //$array[5]="e"; 
-    echo "<p>";
-	echo "<br>\$boolean = ";
-	var_dump ($boolean);
-    echo "<br>\$integer = ".$integer; 
-    echo "<br>\$string = ".$string;// вывод на экран $string = Текст
-    echo "<br>\$float = ".$float;
-	echo "</p>";
+    echo "\$boolean = ",var_dump ($boolean),var_dump((int) $boolean);
+    echo "<br>\$integer = ".$integer." ", var_dump ((bool) $integer);
+    echo "<br>\$string = ".$string." ", var_dump ((bool) $string);
+    echo "<br>\$float = ".$float." ", var_dump ((bool) $float);
+	echo "<br>\$array = ", print_r($array), var_dump ((bool) $array);
 	// можно и так:
-    echo "<br>\$array[0] = ".$array[0];
-    echo "<br>\$array[1] = ".$array[1];
-    echo "<br>\$array[2] = ".$array[2];
-    echo "<br>\$array[3] = ".$array[3];
-    echo "<br>\$array[4] = ".$array[4];
-    echo "<br>\$array[5] = ".$array[5];
-    echo "<br>";
+ //	echo "<p>";
+ //   echo "<br>\$array[0] = ".$array[0];
+ //   echo "<br>\$array[1] = ".$array[1];
+ //   echo "<br>\$array[2] = ".$array[2];
+ //   echo "<br>\$array[3] = ".$array[3];
+ //   echo "<br>\$array[4] = ".$array[4];
+ //   echo "<br>\$array[5] = ".$array[5];
+ //   echo "</p>";
 	//можно и так
-	  print "<pre>"; 
-	  print_r($array);
-	  print "</pre>";
-    
-?>
+//	  print "<pre>"; 
+//	  print_r($array);
+//	  print "</pre>";
+      echo "<p>";
+	  echo "<br>' '-", var_dump((bool) "");        
+      echo "<br>1-", var_dump((bool) 1); 
+      echo "<br>-2-", var_dump((bool) -2);  
+      echo "<br>eehhaa-", var_dump((bool) "eehhaa");
+      echo "<br>false-", var_dump((bool) "false");
+	  echo "<br>array()-", var_dump ((bool) array());
+      echo "</p>";
+   // $b = true;
+	//if ($b = true)
+	//{
+	//echo "$b";
+	//}
+	//else 
+	//{
+	//echo "$b";
+	//}
+	 $b = false;
+     echo $b ? 'true' : 'false'; //will output false
+	?>
+	
 
